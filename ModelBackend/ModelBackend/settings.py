6323a6d1e21f7e7dbf57f186033dd16d916bbb8d
HordesOfGhost <85671929+HordesOfGhost@users.noreply.github.com>
2022-11-19 16:53:27 +0545

    cors headers

--- ModelBackend/ModelBackend/settings.py
+++ ModelBackend/ModelBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +124,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+CORS_ORIGIN_ALLOW_ALL=True
